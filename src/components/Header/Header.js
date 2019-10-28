bc426b5e55d933827e17597e0a2681781a6997ea
Tyler Fann <tyler.fann@cvshealth.com>
2019-10-28 17:33:44 -0400

    created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header-background">
+      <h1 className="header-text">TODO app</h1>
+    </header>
+  );
+}
