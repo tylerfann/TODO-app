f02fe758d03d5b85e283f56230c389a8e5a1fdea
Tyler Fann <tyler.fann@cvshealth.com>
2019-10-28 17:00:26 -0400

    Added css loader to webpack config

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>TODO App</h1>
+        <h1 className='color'>TODO App</h1>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,15 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: 'babel-loader'
+
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
